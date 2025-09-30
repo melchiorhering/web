4abed9603bee582512ad21dc62d6062608742b41
Stijn <stijn@hering.nl>
2025-09-30 15:37:17 +0200

    feat: updating

---
+++ template/src/content.config.ts
@@ -0,0 +1,47 @@
+import { defineCollection, z } from "astro:content";
+
+const heroConfig = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      button: z.object({
+        text: z.string(),
+        href: z.string(),
+      }),
+      heroImage: z.object({
+        src: image(),
+        alt: z.string(),
+      }),
+      bauhausPipes: z.array(
+        z.object({
+          length: z.string(),
+          colorIndex: z.number(),
+        }),
+      ),
+    }),
+});
+
+const woodWork = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      // Add the new field for the background image
+      cover: z.object({ src: image(), alt: z.string() }),
+      // The existing entries array
+      entries: z.array(
+        z.object({
+          title: z.string(),
+          alt: z.string(),
+          cover: image(),
+        }),
+      ),
+    }),
+});
+
+export const collections = {
+  hero: heroConfig,
+  "wood-work": woodWork,
+};
