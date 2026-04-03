55d5f14b4f8d909ee9fa688e3d3def8a2168f022
Stijn <stijn@hering.nl>
2026-04-03 11:14:39 +0200

    feat: refactor

--- bun/houtwerk.hering.services/astro.config.mjs
+++ bun/houtwerk.hering.services/astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://houtwerk.hering.services",
   vite: {
     plugins: [tailwindcss()],
   },
